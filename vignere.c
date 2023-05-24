d2f9a5b852dc3a76476cf9dc541153798c8eb422
neverthelessme <122379541+neverthelessme@users.noreply.github.com>
2023-05-24 23:29:24 +0530

    Update vignere.c

--- vignere.c
+++ vignere.c
@@ -3,48 +3,33 @@
  
 int main(){
     char msg[30],key[30];
-    printf("\n enter the plain text\n");
+    printf("Enter the plaintext:");
     scanf("%s",msg);
-    printf("\n enter the cipher text \n");
+    printf("Enter the key:");
     scanf("%s",key);
     int msgLen = strlen(msg), keyLen = strlen(key), i, j;
-    char a[27][27];
+    char A[27][27];
     for(i=0;i<26;i++){
     for(j=0;j<26;j++){
-    a[i][j]='A'+((j+i)%26);
-    }
-    }
+    A[i][j]='A'+((j+i)%26);}}
     for(i=0;i<26;i++){
     for(j=0;j<26;j++){
-    printf("%c",a[i][j]);
-    }
-    printf("\n");
-    }
- 
+    printf("%c",A[i][j]);}
+    printf("\n");}
     char newKey[msgLen], encryptedMsg[msgLen], decryptedMsg[msgLen];
- 
-    
     for(i = 0, j = 0; i < msgLen; ++i, ++j){
         if(j == keyLen)
             j = 0;
  
         newKey[i] = key[j];
     }
- 
     newKey[i] = '\0';
- 
-    
-    for(i = 0; i < msgLen; ++i)
+            for(i = 0; i < msgLen; ++i)
         encryptedMsg[i] = ((msg[i] + newKey[i]) % 26) + 'A';
- 
     encryptedMsg[i] = '\0';
- 
-    
     for(i = 0; i < msgLen; ++i)
         decryptedMsg[i] = (((encryptedMsg[i] - newKey[i]) + 26) % 26) + 'A';
- 
     decryptedMsg[i] = '\0';
- 
     printf("Original Message: %s", msg);
     printf("\nKey: %s", key);
     printf("\nNew Generated Key: %s", newKey);
